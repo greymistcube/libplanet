d513aa040397dea6f1673fd73b03d7ceec5b4e04
Say Cheong <greymistcube@gmail.com>
2023-10-28 19:52:12 +0900

    Fixed trie key conversion

--- Libplanet.Action/State/KeyConverters.cs
+++ Libplanet.Action/State/KeyConverters.cs
@@ -12,38 +12,10 @@ namespace Libplanet.Action.State
 
         private const byte _underScore = 95;  // '_'
 
-        private static readonly byte[] _conversionTable =
-        {
-            48,  // '0'
-            49,  // '1'
-            50,  // '2'
-            51,  // '3'
-            52,  // '4'
-            53,  // '5'
-            54,  // '6'
-            55,  // '7'
-            56,  // '8'
-            57,  // '9'
-            97,  // 'a'
-            98,  // 'b'
-            99,  // 'c'
-            100, // 'd'
-            101, // 'e'
-            102, // 'f'
-        };
-
         // $"{ByteUtil.Hex(address.ByteArray)}"
         internal static KeyBytes ToStateKey(Address address)
         {
-            var addressBytes = address.ByteArray;
-            byte[] buffer = new byte[addressBytes.Length * 2];
-            for (int i = 0; i < addressBytes.Length; i++)
-            {
-                buffer[i * 2] = _conversionTable[addressBytes[i] >> 4];
-                buffer[i * 2 + 1] = _conversionTable[addressBytes[i] & 0xf];
-            }
-
-            return new KeyBytes(buffer);
+            return new KeyBytes(address.ByteArray);
         }
 
         // $"_{ByteUtil.Hex(address.ByteArray)}_{ByteUtil.Hex(currency.Hash.ByteArray)}"
@@ -51,21 +23,19 @@ namespace Libplanet.Action.State
         {
             var addressBytes = address.ByteArray;
             var currencyBytes = currency.Hash.ByteArray;
-            byte[] buffer = new byte[addressBytes.Length * 2 + currencyBytes.Length * 2 + 2];
+            byte[] buffer = new byte[addressBytes.Length + currencyBytes.Length + 2];
 
             buffer[0] = _underScore;
             for (int i = 0; i < addressBytes.Length; i++)
             {
-                buffer[1 + i * 2] = _conversionTable[addressBytes[i] >> 4];
-                buffer[1 + i * 2 + 1] = _conversionTable[addressBytes[i] & 0xf];
+                buffer[1 + i] = addressBytes[i];
             }
 
-            var offset = addressBytes.Length * 2;
+            var offset = addressBytes.Length;
             buffer[offset + 1] = _underScore;
             for (int i = 0; i < currencyBytes.Length; i++)
             {
-                buffer[offset + 2 + i * 2] = _conversionTable[currencyBytes[i] >> 4];
-                buffer[offset + 2 + i * 2 + 1] = _conversionTable[currencyBytes[i] & 0xf];
+                buffer[offset + 2 + i] = currencyBytes[i];
             }
 
             return new KeyBytes(buffer);
@@ -79,15 +49,14 @@ namespace Libplanet.Action.State
         internal static KeyBytes ToTotalSupplyKey(Currency currency)
         {
             var currencyBytes = currency.Hash.ByteArray;
-            byte[] buffer = new byte[currencyBytes.Length * 2 + 2];
+            byte[] buffer = new byte[currencyBytes.Length + 2];
 
             buffer[0] = _underScore;
             buffer[1] = _underScore;
 
             for (int i = 0; i < currencyBytes.Length; i++)
             {
-                buffer[2 + i * 2] = _conversionTable[currencyBytes[i] >> 4];
-                buffer[2 + i * 2 + 1] = _conversionTable[currencyBytes[i] & 0xf];
+                buffer[2 + i] = currencyBytes[i];
             }
 
             return new KeyBytes(buffer);
